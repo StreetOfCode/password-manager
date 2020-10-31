554e7e27aadf88a17188aff1b3b9b5e0e05266bd
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2020-10-31 22:24:30 +0100

    Add postgresql dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.h2database:h2")
-	
+	implementation("org.postgresql:postgresql")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
