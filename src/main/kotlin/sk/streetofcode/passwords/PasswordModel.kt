10e2e1f48edf3636538abb0cb54f47493f9d6185
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2020-10-31 15:39:52 +0100

    Add PasswordModel

---
+++ src/main/kotlin/sk/streetofcode/passwords/PasswordModel.kt
@@ -0,0 +1,17 @@
+package sk.streetofcode.passwords
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "passwords")
+class PasswordModel(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null,
+
+    var username: String,
+    var password: String,
+    var url: String
+) {
+    constructor() : this(null, "", "", "")
+}
